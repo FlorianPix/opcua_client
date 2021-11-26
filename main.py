d2addedceaa4e7ebc7b6dbfca03b6432b5eb3930
RatedARRR <florian.pix.97@outlook.com>
2021-11-26 23:20:03 +0100

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from opcua import Client
+from opcua import ua
+
+
+def main():
+    with Client("opc.tcp://141.30.154.211:4850") as client:
+        client.connect()
+        client.load_type_definitions()
+        root = client.get_root_node()
+        objects = client.get_objects_node()
+        idx = client.get_namespace_index("http://141.30.154.212:8087/OPC/DA")
+        for child in root.get_children():
+            if child.nodeid.Identifier == 85:
+                for chil in child.get_children():
+                    if chil.nodeid.Identifier == 'XML DA Server - eats11Root':
+                        for chi in chil.get_children():
+                            if chi.nodeid.Identifier == 'F:Schneider':
+                                for ch in chi.get_children():
+                                    try:
+                                        print(ch.nodeid.Identifier, ch.get_value())
+                                    except:
+                                        print(ch)
+
+
+if __name__ == "__main__":
+    main()
